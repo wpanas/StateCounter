da11856729437b5e9fc74ddf977cf94b844babc5
wpanas <wpanas@users.noreply.github.com>
2019-05-12 16:07:18 +0200

    Remove redundant dependency injection

--- app/src/main/java/com/github/wpanas/statecounter/MainActivity.kt
+++ app/src/main/java/com/github/wpanas/statecounter/MainActivity.kt
@@ -14,7 +14,6 @@ import com.github.wpanas.statecounter.action.ActionViewModel
 import com.github.wpanas.statecounter.counter.CounterChanger
 import com.github.wpanas.statecounter.counter.CounterViewModel
 import com.github.wpanas.statecounter.infra.ItemClickListener
-import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.DaggerAppCompatActivity
@@ -35,7 +34,6 @@ class MainActivity : DaggerAppCompatActivity(), ItemClickListener, CounterChange
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
