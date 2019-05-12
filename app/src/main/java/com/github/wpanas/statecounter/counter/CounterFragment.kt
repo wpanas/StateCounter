d921b8cdf63a319f66b7dcc6d9cbff7da8446760
wpanas <wpanas@users.noreply.github.com>
2019-05-12 17:40:52 +0200

    Fix fragment onclick missing methods

--- app/src/main/java/com/github/wpanas/statecounter/counter/CounterFragment.kt
+++ app/src/main/java/com/github/wpanas/statecounter/counter/CounterFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import com.github.wpanas.statecounter.R
@@ -33,10 +34,20 @@ class CounterFragment : DaggerFragment() {
         val view = inflater.inflate(R.layout.fragment_counter, container, false)
         val counter = view?.findViewById<TextView>(R.id.counter)
 
+        initCounterChangerListeners(view)
+
         counterViewModel.counter.observe(this, Observer {
             counter?.text = it.toString()
         })
 
         return view
+    }
+
+    private fun initCounterChangerListeners(view: View) {
+        val counterChanger: CounterChanger = context as CounterChanger
+
+        view.findViewById<Button>(R.id.minus_button).setOnClickListener(counterChanger::decrementState)
+        view.findViewById<Button>(R.id.plus_button).setOnClickListener(counterChanger::incrementState)
+        view.findViewById<Button>(R.id.save_button).setOnClickListener(counterChanger::saveState)
     }
 }
